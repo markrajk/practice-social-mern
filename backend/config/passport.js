fba85a15321e05085b575b643f9ac2bedc3fd6d0
markrajk <marko88rajkovic@gmail.com>
2021-05-05 10:34:37 +0200

    fifth commit

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -18,7 +18,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'http://localhost:5000/api/auth/google/callback',
+      callbackURL:
+        'https://practice-social-mern.herokuapp.com/api/auth/google/callback',
       proxy: true,
     },
     function (accessToken, refreshToken, profile, done) {
